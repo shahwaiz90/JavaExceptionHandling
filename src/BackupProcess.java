9a6090cb572d490b3274a59f84fd1cd9e1f95e2c
Ahmad Shahwaiz <shahwaiz90@yahoo.com>
2019-02-13 03:23:09 +0500

    Added Checked, Unchecked and Custom Exception

--- src/BackupProcess.java
+++ src/BackupProcess.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+
 public class BackupProcess {
 
-    void someErrorThrowingFunction() throws SessionExpireException, ArithmeticException {
+    void someErrorThrowingFunction() throws IOException, ArithmeticException, SessionExpireException {
         //Gets server response, in server response: there wasn't any user object. Which means its session is expired.
         SessionExpireException sessionExpireException = new SessionExpireException("SessionExpired");
         sessionExpireException.initCause(new NullPointerException("No User Object Found"));
         throw sessionExpireException;
+//        throw new IOException("io");
+//        throw new ArithmeticException("arithimetic");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,24 @@ public class Main {
             backupProcess.someErrorThrowingFunction();
             return;
         }
-        catch (Exception e) {
+        catch (SessionExpireException e) {
             if(e.getClass().getCanonicalName().equals("SessionExpireException")){
                 //Handling....!!!!
                 // logout the user and show appropriate message that his session has been expired.
                 //Delete user database
                 System.out.println("SessionExpireException Exception Occurred!: "+e.getMessage());
                 System.out.println("SessionExpireException Exception Cause!: "+e.getCause());
+                //may be send the cause to server for further debugging.
 
             }
-             if(e.getMessage()!=null&& e.getMessage().equals("NPE")){
-                //Handling....!!!
-                //Initialize that particular object if it is null or show appropriate message.
-                //Handling is done according to the scenario.
-                System.out.println("NullPointerException Exception Occurred!: "+e.getCause());
-
-            }
-        }finally{
+        }
+        catch (NullPointerException e){
+            System.out.println("Null pointer Exception Occurred!: "+e.getMessage());
+        }
+        catch (Exception e){
+            System.out.println("Other Exception Occurred!: "+e.getMessage());
+        }
+        finally{
             //Here we close any db connections we opened above. etc
             //This blocks always executes.
             System.out.println("This code always execute");
